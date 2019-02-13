abaf0781cd818e27a5f3e21a2c8de432f9cd4f88
cperez604 <47163795+cperez604@users.noreply.github.com>
2019-02-13 17:15:13 -0600

    Add files via upload

---
+++ CFPbusinesscard/CFPbusinesscard/CFPbusinesscard.cpp
@@ -0,0 +1,20 @@
+// CFPbusinesscard.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+	int main()
+		//
+{
+	cout << "Name: Fernando Perez" << endl;
+	cout << "Phone: 210-456-8901" << endl;
+	cout << "Email: leprogrammette@hotmail.com" << endl;
+	cout << "Contact me for any further information" << endl;
+	cout << "Available weekday afternoons" << endl;
+
+	return 0;
+
+}
